3a5d3dea6a2c9b8b404c5f1d7af36e19b3402ed6
donriddo <donriddo@gmail.com>
2017-10-01 13:28:37 +0100

    Adds Setup model for application bootstrap

---
+++ api/models/Setup.js
@@ -0,0 +1,16 @@
+/**
+* Setup.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+*/
+
+module.exports = {
+
+    attributes: {
+        saasInitialized: {
+            type: 'boolean',
+            defaultsTo: false,
+        },
+    },
+};
